c0ce566ec8d8afb0372612beb304bb764a55579b
Marcin Orlinski <orzel299@gmail.com>
2024-05-17 11:43:16 +0200

    Added cloud run tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -21,20 +21,7 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
+module "cloud_run" {
+  source  = "../../modules/cloud_run"
   project = "${var.project}"
-  env     = "${local.env}"
-}
-
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
 }
---
+++ modules/cloud_run/main.tf
@@ -0,0 +1,26 @@
+resource "google_cloud_run_v2_service" "default" {
+  name     = "ticket-review-app"
+  location = "us-central1"
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "gcr.io/${var.project}/ticket-review-app"
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "1024Mi"
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].containers[0].image,
+      template[0].containers[0].name,
+      client,
+      client_version
+    ]
+  }
+}
