1a2894085eee53e501fd54d3502d820b4ef118b8
Marcin Orlinski <orzel299@gmail.com>
2024-05-17 17:49:17 +0200

    forgot to add this

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -21,7 +21,27 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "cloud_run" {
-  source  = "../../modules/cloud_run"
-  project = "${var.project}"
+resource "google_compute_global_address" "default" {
+  name = "global-ticket-review-ip"
+}
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "ticket-review-app"
+  location = "us-central1"
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "gcr.io/${var.project}/ticket-review-app"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].containers[0].image,
+      template[0].containers[0].name,
+      client,
+      client_version
+    ]
+  }
 }
