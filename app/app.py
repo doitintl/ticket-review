d5ee1c8021ba3cf21927258768bbe614cdd47e7e
Marcin Orlinski <orzel299@gmail.com>
2024-05-27 17:11:00 +0200

    testing headers

--- app/app.py
+++ app/app.py
@@ -1,9 +1,43 @@
 import streamlit as st
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+from google.auth.transport import requests
+from google.oauth2 import id_token
+
+
+def validate_iap_jwt(iap_jwt, expected_audience):
+    """Validate an IAP JWT.
+
+    Args:
+      iap_jwt: The contents of the X-Goog-IAP-JWT-Assertion header.
+      expected_audience: The Signed Header JWT audience. See
+          https://cloud.google.com/iap/docs/signed-headers-howto
+          for details on how to get this value.
+
+    Returns:
+      (user_id, user_email, error_str).
+    """
+
+    try:
+        decoded_jwt = id_token.verify_token(
+            iap_jwt,
+            requests.Request(),
+            audience=expected_audience,
+            certs_url="https://www.gstatic.com/iap/verify/public_key",
+        )
+        return (decoded_jwt["sub"], decoded_jwt["email"], "")
+    except Exception as e:
+        return (None, None, f"**ERROR: JWT validation error {e}**")
 
 def main():
+
+    headers = _get_websocket_headers()
+    access_token = headers.get("x-goog-iap-jwt-assertion")
+
     st.title('Ticket Review App')
 
     # Text area for ticket input
+    ticket_text = st.text_area('Headers', headers)
+
     ticket_text = st.text_area('Ticket Text', 'Enter the ticket text here...')
 
     # Text area for reviewer's thoughts
