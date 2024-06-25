4d3b984756e722807b2faea284a7b036c9d235a3
Marcin Orlinski <orzel299@gmail.com>
2024-06-25 15:34:33 +0200

    Adding firestore to terraform

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -39,6 +39,13 @@ resource "google_project_iam_member" "cloud-run-sa-role-attachment" {
   project = var.project
 }
 
+resource "google_firestore_database" "database" {
+  project     = var.project
+  name        = "(default)"
+  location_id = var.region
+  type        = "FIRESTORE_NATIVE"
+}
+
 resource "google_cloud_run_v2_service" "default" {
   name     = "${var.app_name}-app"
   location = var.region
